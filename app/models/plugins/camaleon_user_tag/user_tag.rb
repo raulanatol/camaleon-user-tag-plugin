b3cbc8b04d56b8c393dc8280b0b0e69d49d4a383
raulanatol <raul@natol.es>
2016-11-11 15:18:09 +0000

    Upgrade attr_accesible methods

--- app/models/plugins/camaleon_user_tag/user_tag.rb
+++ app/models/plugins/camaleon_user_tag/user_tag.rb
@@ -1,5 +1,14 @@
 class Plugins::CamaleonUserTag::UserTag < ActiveRecord::Base
   self.table_name = 'plugins_camaleon_user_tag_user_tags'
 
-  attr_accessible :name, :counter
+  def create
+    @user = UserTag.new(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :counter)
+  end
+
 end
